19d8c34ddb041b5e4d1a9cc32f956093e69879a6
Alexandr Yakubenko <alexandr.yakubenko@gmail.com>
2015-03-30 12:41:26 +0300

    added unicorn-zero-downtime

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,19 +1,18 @@
-# config valid only for current version of Capistrano
 lock '3.3.5'
 
 server 'safehands.by', user: 'deployer'
 
+set :password, 'SanityEclipse32;'
+
 set :application, 'safehands'
 set :repo_url, 'git@github.com:alexyakubenko/safehands.git'
 
-# Default value for :pty is false
-# set :pty, true
+#set :pty, true
 
 set :linked_files, fetch(:linked_files, []).push('config/database.yml')
 
 set :rvm_ruby_version, 'ruby-head@safehands'
 
-# Default value for linked_dirs is []
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids')
 
 # Default value for default_env is {}
--- config/unicorn.rb
+++ config/unicorn.rb
@@ -7,3 +7,25 @@ stdout_path "#{ root }/log/unicorn.log"
 listen '/tmp/unicorn.safehands.sock'
 worker_processes 4
 timeout 30
+
+preload_app true
+
+after_fork do |server, worker|
+  db_config = YAML::load(File.open(File.join(settings.root, 'config', 'database.yml')))
+  ActiveRecord::Base.establish_connection(db_config)
+end
+
+before_fork do |server, worker|
+  if defined?(ActiveRecord::Base)
+    ActiveRecord::Base.connection.disconnect!
+  end
+
+  old_pid = "#{ server.config[:pid] }.oldbin"
+  if File.exists?(old_pid) && server.pid != old_pid
+    begin
+      Process.kill('QUIT', File.read(old_pid).to_i)
+    rescue Errno::ENOENT, Errno::ESRCH
+      # someone else did our job for us
+    end
+  end
+end
