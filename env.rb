542cf23d0daf070de8061b30382d9138b8d8103a
Alexandr Yakubenko <alexandr.yakubenko@gmail.com>
2015-04-06 00:24:33 +0300

    ensure that ar-connection was established (in case of rake task running)

--- env.rb
+++ env.rb
@@ -1,1 +1,8 @@
 require 'sinatra'
+
+unless ActiveRecord::Base.connected?
+  db_config = YAML::load(File.open(File.join(settings.root, 'config', 'database.yml')))[env]
+
+  ActiveRecord::Base.establish_connection(db_config)
+  ActiveRecord::Base.logger = Logger.new(STDOUT)
+end
