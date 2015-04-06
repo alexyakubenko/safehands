c52db3d4f591771491c3ca9d98eddbb97d463e51
Alexandr Yakubenko <alexandr.yakubenko@gmail.com>
2015-04-06 10:53:18 +0300

    use absolute path for unicorn root directory

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,4 +1,4 @@
-root = '~/apps/safehands/current'
+root = '/home/deployer/apps/safehands/current'
 working_directory root
 pid "#{ root }/tmp/pids/unicorn.pid"
 stderr_path "#{ root }/log/unicorn_out.log"
