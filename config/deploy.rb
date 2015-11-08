65c114423ffcb6a1e941ef9400f458559db28502
Alex Yakubenko <alexandr.yakubenko@gmail.com>
2015-11-08 23:34:52 +0300

    added twilio symlink on deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -35,6 +35,7 @@ namespace :deploy do
 
   task :symlink_config, roles: :app do
     run "ln -nfs #{ shared_path }/config/database.yml #{ release_path }/config/database.yml"
+    run "ln -nfs #{ shared_path }/config/twilio.yml #{ release_path }/config/twilio.yml"
   end
 
   after 'deploy:finalize_update', 'deploy:symlink_config'
